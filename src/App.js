7e635bdc61919464bf17a9079ff1cf3c6b989ab0
lobo <lobo1999cr@gmail.com>
2022-06-24 18:48:36 -0600

    Connecting components through Link and Router

--- src/App.js
+++ src/App.js
@@ -3,8 +3,23 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
 import Register from "./components/Register";
-//import AdminMainPage from "./components/AdminMainPage";
-//<Route path="/adminDashboard" element={<AdminMainPage />} />
+import AdminMainPage from "./components/AdminMainPage";
+import {
+  Ecommerce,
+  Orders,
+  Calendar,
+  Stacked,
+  Pyramid,
+  Customers,
+  Kanban,
+  Line,
+  Area,
+  Bar,
+  Pie,
+  index,
+  Financial,
+  ColorMapping,
+} from "./pages";
 
 export const App = () => {
   return (
@@ -13,6 +28,20 @@ export const App = () => {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/adminDashboard" element={<AdminMainPage />} />
+          <Route path="/ecommerce" element={<Ecommerce />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/stacked" element={<Stacked />} />
+          <Route path="/pyramid" element={<Pyramid />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/kanban" element={<Kanban />} />
+          <Route path="/line" element={<Line />} />
+          <Route path="/area" element={<Area />} />
+          <Route path="/bar" element={<Bar />} />
+          <Route path="/pie" element={<Pie />} />
+          <Route path="/financial" element={<Financial />} />
+          <Route path="/colorMapping" element={<ColorMapping />} />
         </Routes>
       </Router>
     </div>
--- src/components/AdminMainPage.jsx
+++ src/components/AdminMainPage.jsx
@@ -1,90 +1,68 @@
 import React, { useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import { Navbar, Footer, ThemeSettings, Sidebar } from "./";
-import {
-  Ecommerce,
-  Orders,
-  Calendar,
-  Stacked,
-  Pyramid,
-  Customers,
-  Kanban,
-  Line,
-  Area,
-  Bar,  
-  Pie,
-  index,
-  Financial,
-  ColorMapping,
-} from "../pages";
 
-export const AdminMainPage = () => {
+
+export default function AdminMainPage() {
   const activeMenu = true;
 
   return (
     <div>
-      <BrowserRouter>
-        <div className="flex relative dark:bg-main-dark-bg">
-          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
-            <TooltipComponent content="Settings" position="Top">
-              <button
-                type="button"
-                className="text-3xl p-3 hover:drop-shawdow-xl hover:bg-light-gray"
-                style={{ background: "blue", borderRadius: "50%" }}
-              >
-                <FiSettings />
-              </button>
-            </TooltipComponent>
+      <div className="flex relative dark:bg-main-dark-bg">
+        <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+          <TooltipComponent content="Settings" position="Top">
+            <button
+              type="button"
+              className="text-3xl p-3 hover:drop-shawdow-xl hover:bg-light-gray"
+              style={{ background: "blue", borderRadius: "50%" }}
+            >
+              <FiSettings />
+            </button>
+          </TooltipComponent>
+        </div>
+        {activeMenu ? (
+          <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+            Sidebar
           </div>
-          {activeMenu ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-              Sidebar
-            </div>
-          ) : (
-            <div className="w-0 dark:bg-secondary-dark-bg">Sidebar w-0</div>
-          )}
-          <div
-            className={`dark:main-bg bg-main-bg min-h-screen w-full ${
-              activeMenu ? "md:ml-72 w-full" : "w-full flex-2"
-            }`}
-          >
-            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-              Navbar
-            </div>
-          </div>
-          <div>
-            <Routes>
-              {/* Dashboard principal */}
-              <Route path="/" element="ECommerce" />
-              <Route path="/ecommerce" element="ECommerce" />
-
-              {/* Páginas*/}
-              <Route path="/orders" element="Orders" />
-              <Route path="/customers" element="Customers" />
-              <Route path="/ingredients" element="Ingredients" />
-
-              {/* Apps */}
-              <Route path="/kanban" element="Kanban" />
-              <Route path="/calendar" element="Calendar" />
-              <Route path="/editor" element="Editor" />
-
-              {/* Gráficos */}
-              <Route path="/line" element="Line" />
-              <Route path="/area" element="Area" />
-              <Route path="/bar" element="Bar" />
-              <Route path="/pie" element="Pie" />
-              <Route path="/financial" element="Financial" />
-              <Route path="/color-mapping" element="ColorMapping" />
-              <Route path="/pyramid" element="Pyramid" />
-              <Route path="/stacked" element="Stacked" />
-            </Routes>
+        ) : (
+          <div className="w-0 dark:bg-secondary-dark-bg">Sidebar w-0</div>
+        )}
+        <div
+          className={`dark:main-bg bg-main-bg min-h-screen w-full ${
+            activeMenu ? "md:ml-72 w-full" : "w-full flex-2"
+          }`}
+        >
+          <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+            Navbar
           </div>
         </div>
-      </BrowserRouter>
+        <div>
+          {/* Dashboard principal */}
+          <Link to="/ecommerce">Ecommerce</Link>
+
+          {/* Páginas*/}
+          <Link to="/orders">Orders</Link>
+          <Link to="/customers">Customers</Link>
+          <Link to="/ingredients">Ingredients</Link>
+
+          {/* Apps */}
+          <Link to="/kanban">Kanban</Link>
+          <Link to="/calendar">Calendar</Link>
+          <Link to="/editor">Editor</Link>
+
+          {/* Gráficos */}
+          <Link to="/line">Line</Link>
+          <Link to="/area">Area</Link>
+          <Link to="/bar">Bar</Link>
+          <Link to="/pie">Pie</Link>
+          <Link to="/financial">Financial</Link>
+          <Link to="/colorMapping">Color Mapping</Link>
+          <Link to="/pyramid">Pyramid</Link>
+          <Link to="/stacked">Stacked</Link>
+        </div>
+      </div>
     </div>
   );
-};
-
-export default AdminMainPage;
+}
